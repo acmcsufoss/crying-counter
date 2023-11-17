566c102848ad504246c47a6a7750483df90e8a60
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-17 14:26:45 -0800

    use levenshtein algorithm

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 	"fmt"
 
 	"github.com/acmcsufoss/crying-counter/matcher"
+	"github.com/texttheater/golang-levenshtein/levenshtein"
 )
 
 func main() {
-	m := matcher.NewMatcher()
-	result, err := m.Match("hello world", matcher.MatchOptions{
+	m := matcher.Matcher{
 		Phrases: []matcher.Phrase{
 			{
 				"hello",
@@ -20,12 +20,9 @@ func main() {
 		InteruptThreshold:   1,
 		SimilarityThreshold: 0.5,
 		Similarity: func(a matcher.Word, b matcher.Word) float64 {
-			return 1
+			return levenshtein.RatioForStrings([]rune(a), []rune(b), levenshtein.DefaultOptions)
 		},
-	})
-	if err != nil {
-		panic(err)
 	}
-
+	result := m.Match("hello world")
 	fmt.Println(result)
 }
--- matcher/matcher.go
+++ matcher/matcher.go
@@ -3,18 +3,7 @@ package matcher
 import "strings"
 
 // Matcher is the interface that wraps the Match method.
-type Matcher interface {
-	Match(string, MatchOptions) (bool, error)
-}
-
-// Word is a string that represents a word.
-type Word string
-
-// Phrase is a list of words that represents a phrase.
-type Phrase []Word
-
-// MatchOptions is the options used to match a phrase against a content.
-type MatchOptions struct {
+type Matcher struct {
 	// Phrases is a list of phrases to compare against the content.
 	Phrases []Phrase
 
@@ -29,13 +18,11 @@ type MatchOptions struct {
 	Similarity func(Word, Word) float64
 }
 
-// NewMatcher returns a new Matcher.
-func NewMatcher() Matcher {
-	return &matcher{}
-}
+// Word is a string that represents a word.
+type Word string
 
-// matcher is the default implementation of the Matcher interface.
-type matcher struct{}
+// Phrase is a list of words that represents a phrase.
+type Phrase []Word
 
 // Words converts a string into a list of words.
 func Words(content string) (words []Word) {
@@ -47,39 +34,37 @@ func Words(content string) (words []Word) {
 }
 
 // Match returns true if the content matches the phrases.
-func (m *matcher) Match(content string, options MatchOptions) (bool, error) {
+func (m *Matcher) Match(content string) bool {
 	words := Words(content)
-	for _, phrase := range options.Phrases {
-		if match, err := matchPhrase(words, phrase, options); err != nil {
-			return false, err
-		} else if match {
-			return true, nil
+	for _, phrase := range m.Phrases {
+		if m.MatchPhrase(words, phrase) {
+			return true
 		}
 	}
 
-	return false, nil
+	return false
 }
 
 // matchPhrase returns true if the words match the phrase.
-func matchPhrase(words []Word, phrase Phrase, options MatchOptions) (bool, error) {
+func (m *Matcher) MatchPhrase(words []Word, phrase Phrase) bool {
 	i, latestInteruptIndex := 0, -1
 	for _, word := range words {
 		// If the word is similar to the current word in the phrase, we continue.
-		if options.Similarity(word, phrase[i]) >= options.SimilarityThreshold {
+		if m.Similarity(word, phrase[i]) >= m.SimilarityThreshold {
 			i++
 		}
 
 		// If the word is not similar to the current word in the phrase, we check
 		// if we can skip it.
-		if latestInteruptIndex > -1 && latestInteruptIndex+options.InteruptThreshold < i {
-			return false, nil
+		if latestInteruptIndex > -1 && latestInteruptIndex+m.InteruptThreshold < i {
+			return false
 		}
 
 		// If we reached the end of the phrase, we return true.
 		if i == len(phrase) {
-			return true, nil
+			return true
 		}
 	}
 
-	return false, nil
+	return false
 }
